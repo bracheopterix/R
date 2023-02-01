ca55717ebd1df8e4ac6e3fc0095ee6e4923a15a4
bracheopterix <satoru-kun@mail.ru>
2023-02-01 06:09:51 +0200

    started to move header animation to css from js

--- script/header_minimizer.js
+++ script/header_minimizer.js
@@ -10,25 +10,27 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        header.style.maxHeight =  "3em";
-        header.style.padding =  "0 20em 0 20em";
-        avatar.style.height = "3em";
-        avatar.style.width = "3em";
-        menuButton.style.height = "3em";
-        menuButton.style.width = "3em";
-        menuButton.style.top = "0";
-        menuButton.style.backgroundSize = "3em";
-        menuButton.style.opacity ="1";
+        header.classList.toggle ("headerbox-inner_mini");
+        // header.style.maxHeight =  "3em";
+        // header.style.padding =  "0 20em 0 20em";
+        // avatar.style.height = "3em";
+        // avatar.style.width = "3em";
+        // menuButton.style.height = "3em";
+        // menuButton.style.width = "3em";
+        // menuButton.style.top = "0";
+        // menuButton.style.backgroundSize = "3em";
+        // menuButton.style.opacity ="1";
 
     }
     else {
-        header.style.maxHeight =  "fit-content";
-        header.style.padding =  "5em 20em 5em 20em";
-        avatar.style.height = "8em";
-        avatar.style.width = "8em";
-        menuButton.style.height = "8em";
-        menuButton.style.width = "8em";
-        menuButton.style.top = "5em";
+        header.classList.toggle ("headerbox-inner_mini");
+        // header.style.maxHeight =  "fit-content";
+        // header.style.padding =  "5em 20em 5em 20em";
+        // avatar.style.height = "8em";
+        // avatar.style.width = "8em";
+        // menuButton.style.height = "8em";
+        // menuButton.style.width = "8em";
+        // menuButton.style.top = "5em";
     }
 } 
 
