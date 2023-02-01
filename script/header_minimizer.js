fc2e3037f8f63f64d9154434e94ee2167379ead0
bracheopterix <satoru-kun@mail.ru>
2023-02-01 09:52:14 +0200

    changing c-puters

--- script/header_minimizer.js
+++ script/header_minimizer.js
@@ -1,15 +1,16 @@
 
 let header = document.getElementById('headerbox');
 let avatar = document.getElementById('headerbox-inner-avatarbox')
-let menuButton = document.getElementById('headerbox-inner-menu_button')
 
 console.log("it`s ok");
-console.log(avatar);
+
+
 
 window.onscroll = function () { scrollFunction() };
 
 
 function scrollFunction() {
+  
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         header.classList.toggle ("mini");
         avatar.classList.toggle ("mini");
@@ -17,7 +18,6 @@ function scrollFunction() {
     else {
         header.classList.toggle ("mini");
         avatar.classList.toggle ("mini");
-        console.log(avatar, "2");
     }
 } 
 
