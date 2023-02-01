a157e0b1c3f2b4746525080d2d8d5bf1e1ad5bca
bracheopterix <satoru-kun@mail.ru>
2023-02-01 07:04:21 +0200

    rewrited classes and ids added css transitions. now the problem is triggering animation with js

--- script/header_minimizer.js
+++ script/header_minimizer.js
@@ -1,36 +1,23 @@
 
 let header = document.getElementById('headerbox');
-let avatar = document.getElementById('headerbox-inner-avatar-img')
+let avatar = document.getElementById('headerbox-inner-avatarbox')
 let menuButton = document.getElementById('headerbox-inner-menu_button')
 
 console.log("it`s ok");
+console.log(avatar);
 
 window.onscroll = function () { scrollFunction() };
 
 
 function scrollFunction() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        header.classList.toggle ("headerbox-inner_mini");
-        // header.style.maxHeight =  "3em";
-        // header.style.padding =  "0 20em 0 20em";
-        // avatar.style.height = "3em";
-        // avatar.style.width = "3em";
-        // menuButton.style.height = "3em";
-        // menuButton.style.width = "3em";
-        // menuButton.style.top = "0";
-        // menuButton.style.backgroundSize = "3em";
-        // menuButton.style.opacity ="1";
-
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.classList.toggle ("mini");
+        avatar.classList.toggle ("mini");
     }
     else {
-        header.classList.toggle ("headerbox-inner_mini");
-        // header.style.maxHeight =  "fit-content";
-        // header.style.padding =  "5em 20em 5em 20em";
-        // avatar.style.height = "8em";
-        // avatar.style.width = "8em";
-        // menuButton.style.height = "8em";
-        // menuButton.style.width = "8em";
-        // menuButton.style.top = "5em";
+        header.classList.toggle ("mini");
+        avatar.classList.toggle ("mini");
+        console.log(avatar, "2");
     }
 } 
 
