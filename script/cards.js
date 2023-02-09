101cc67799af17eaa8ecb484e8bf75d6ad273e20
bracheopterix <satoru-kun@mail.ru>
2023-02-09 23:16:31 +0200

    optimization

--- script/cards.js
+++ script/cards.js
@@ -1,26 +1,29 @@
-const cardVerb = document.getElementById("card-verb");
-const cardMeaning = document.getElementById("card-meaning");
-console.log(cardVerb, cardMeaning);
+window.onload = () => {
+    const cardVerb = document.getElementById("card-verb");
+    const cardMeaning = document.getElementById("card-meaning");
+    console.log(cardVerb, cardMeaning);
 
-const data = [
-    {verb: 1, meaning: 'hello'},
-    {verb: 2, meaning: 'herro'},
-    {verb: 5, meaning: '6'},
-    {verb: 8, meaning: '9'},
-];
+    cardVerb.onclick = () => cardChange();
 
+    const data = [
+        {verb: 1, meaning: 'hello'},
+        {verb: 2, meaning: 'herro'},
+        {verb: 5, meaning: '6'},
+        {verb: 8, meaning: '9'},
+    ];
 
-function cardChange() {
-    console.log("it worked");
-    const foo = getRandomPair();
-    const verb22 = foo.verb;
-    const meaning22 = foo.meaning;
-    console.log(verb22, meaning22);
-    cardVerb.innerHTML = verb22;
-    cardMeaning.innerHTML = meaning22;
-}
+    function cardChange() {
+        console.log("it worked");
+        const foo = getRandomPair();
+        const verb22 = foo.verb;
+        const meaning22 = foo.meaning;
+        console.log(verb22, meaning22);
+        cardVerb.innerHTML = verb22;
+        cardMeaning.innerHTML = meaning22;
+    }
 
-function getRandomPair() {
-    const randomIndex = Math.round(Math.random() * (data.length - 1));
-    return data[randomIndex];
+    function getRandomPair() {
+        const randomIndex = Math.round(Math.random() * (data.length - 1));
+        return data[randomIndex];
+    }
 }
