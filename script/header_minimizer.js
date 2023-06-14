ba7a92e38af412e5ca04bda35a078b6557333478
bracheopterix <satoru-kun@mail.com>
2023-06-14 13:54:31 +0300

    this is changes before I did multiple pages and a header

--- script/header_minimizer.js
+++ script/header_minimizer.js
@@ -1,22 +1,39 @@
 
 let header = document.getElementById('headerbox');
 let avatar = document.getElementById('headerbox-inner-avatarbox')
+let mainInner = document.getElementById('main-inner');
 
-console.log("it`s ok");
+
+console.log(mainInner);
 
 
 
 window.onscroll = function () { scrollFunction() };
 
 
+// function scrollFunction() {
+//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//         header.classList.add ("mini");
+//         avatar.classList.add ("mini");
+//         console.log("added a class");
+//     }
+//     else {
+//         header.classList.remove ("mini");
+//         avatar.classList.remove ("mini");
+//         console.log("deleted a class");
+//     }
+// } 
+
 function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    
         header.classList.add ("mini");
         avatar.classList.add ("mini");
+        console.log("added a class");
     }
     else {
         header.classList.remove ("mini");
         avatar.classList.remove ("mini");
+        console.log("deleted a class");
     }
 } 
-
